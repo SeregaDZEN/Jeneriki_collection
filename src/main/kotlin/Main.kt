f2ba61fef8612a4420cf7159ed91be439fa41adf
Serega <fezarikus@gmail.com>
2023-06-11 18:10:39 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,85 @@
+fun main() {
+
+}
+
+interface Elem {
+    val id: Int
+    var text: String
+}
+
+data class Note(override val id: Int, override var text: String, val comments: MutableList<Comment> = mutableListOf()) :
+    Elem // 1ый дженерик
+
+data class Comment(override val id: Int, override var text: String, var isDelete: Boolean) : Elem // 2дженерик
+
+open class CrudService<T : Elem>(val elems: MutableList<T>) {
+
+    open fun add(elem: T): T {
+        elems += elem
+        return elems.last()
+    }
+
+    fun get(): MutableList<T> = elems
+
+    fun edit(elem: T): T {
+        val editElem = elems.find { it.id == elem.id } ?: throw ElemNotFindException(" не найден по такому айди")
+        if (editElem is Comment && editElem.isDelete) {
+            throw RuntimeException("комментарий удалён!")
+        }
+        editElem.text = elem.text;
+        return editElem
+    }
+
+    fun delete(elemId: Int): Boolean {
+        val searchElem = elems.find { it.id == elemId } ?: return false
+        return if (searchElem is Comment) {
+            searchElem.isDelete = true
+            true
+        } else {
+            elems.remove(searchElem)
+        }
+    }
+
+    fun getById(elemId: Int): T {
+        return elems.find { it.id == elemId } ?: throw ElemNotFindException(" не найден по такому айди")
+    }
+}
+
+class CommentService(list: MutableList<Comment>) : CrudService<Comment>(list) {
+    fun restoreComment(commentId: Int): Boolean {
+        val elemForRestore = elems.find { it.id == commentId } ?: return false
+        return elemForRestore.isDelete
+    }
+}
+
+object NoteService {
+    private val noteService = CrudService<Note>(mutableListOf())
+
+    fun add(elem: Note) = noteService.add(elem)
+    fun addComment(note: Note, comment: Comment) = CommentService(noteService.getById(note.id).comments).add(comment)
+    fun delete(idNote: Int) = noteService.delete(idNote)
+
+
+    fun edit(note: Note) = noteService.edit(note)
+   // fun edit2(note: Note)  : Note {  return noteService.edit(note) }
+
+
+    fun deleteComment(note: Note, comment: Comment) = CommentService(noteService.getById(note.id).comments).delete(comment.id)
+
+    fun editComment(note: Note, comment: Comment) = CommentService(noteService.getById(note.id).comments).edit(comment)
+    fun getNotes(): MutableList<Note> { // уточнить про название, если будет просто гет!
+        return noteService.get()
+    }
+
+    fun getById(idNote: Int): Note {
+        return noteService.getById(idNote)
+    }
+
+    fun getComments(note: Note) = CommentService(noteService.getById(note.id).comments)
+
+}
+
+
+class ElemNotFindException(msg: String) : RuntimeException(msg)
+
+
---
+++ src/test/kotlin/NoteServiceTest.kt
@@ -0,0 +1,137 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
+
+class NoteServiceTest {
+
+
+    @Test
+    fun addPositive() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val result = service.add(note)
+        assertEquals(result, note)
+    }
+
+
+    @Test
+    fun addComment() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val comment = Comment(1, "lala", false)
+        service.add(note)
+
+        val result = service.addComment(note, comment)
+        assertEquals(result.text, comment.text)
+
+    }
+
+
+    @Test
+    fun delete() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val idNote = 1
+        service.add(note)
+        val result = service.delete(idNote)
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun delete2() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val idNote = 1
+        val result = service.delete(idNote)
+
+        assertFalse(result)
+    }
+
+    @Test
+    fun edit() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val note2 = Note(1, "ni")
+        service.add(note)
+        val result = service.edit(note2)
+        assertEquals(result, note2)
+    }
+
+    @Test
+    fun edit2() {
+        val service = NoteService
+        val note = Note(1, "noni")
+
+        service.add(note)
+        val result = service.edit(note)
+        assertEquals(result, note)
+    }
+
+    @Test
+    fun deleteCommentTrue() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val comment = Comment(1, "lala", false)
+        service.add(note)
+        service.addComment(note,comment)
+
+        val result = service.deleteComment(note, comment)
+
+        assertTrue(result)
+    }
+    @Test
+    fun deleteCommentFalse() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val comment = Comment(3, "lala", false)
+        service.add(note)
+        val result = service.deleteComment(note, comment)
+        assertFalse(result)
+    }
+
+    @Test
+    fun editComment() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val comment = Comment(3, "lala", false)
+        service.add(note)
+        service.addComment(note,comment)
+        val result = service.editComment(note, comment)
+        assertEquals(result, comment)
+    }
+
+    @Test
+    fun getNotes() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val note2 = Note(2, "ni")
+        service.add(note)
+        service.add(note2)
+        val result = service.getNotes()
+        assertEquals(result, mutableListOf(note,note2))
+
+    }
+
+    @Test
+    fun getById() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        service.add(note)
+         val idNote = 1
+        val result = service.getById(idNote)
+        assertEquals(result, note)
+    }
+
+    @Test
+    fun getComments() {
+        val service = NoteService
+        val note = Note(1, "noni")
+        val comment = Comment(1, "nni", false)
+        service.add(note)
+        service.addComment(note,comment)
+        val result = service.getComments(note).get()
+        assertEquals(result, mutableListOf(comment))
+    }
+}
